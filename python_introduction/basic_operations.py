32a8524124c7e5957b47986e0f8e458ca23042a4
ghost <fredrickmbithi000@gmail.com>
2025-11-06 18:47:08 +0300

    Task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition
+sum_result = number1 + number2
+print("Addition:", sum_result)
+
+# Subtraction
+difference_result = number1 - number2
+print("Subtraction:", difference_result)
+
+# Multiplication
+product_result = number1 * number2
+print("Multiplication:", product_result)
