97dac846c28ca3aba3aad9b58d7561298bc389fd
FredrickMbithi <fredrickmbithi000@gmail.com>
2025-11-23 02:15:50 +0300

    added fns_and_dsa

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,6 @@ size = int(input("Enter the size of the pattern:"))
 
 row = 0
 
-
 while row < size: 
     for column in range(size):
         print("*", end="")
---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1, num2, operation):
+
+    if operation == "add":
+        return num1 + num2
+
+    elif operation == "subtract":
+        return num1 - num2
+
+    elif operation == "multiply":
+        return num1 * num2
+
+    elif operation == "divide":
+        # Handle division by zero
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+
+    else:
+        # Unknown operation
+        return "Error: Invalid operation"
