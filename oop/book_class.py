b1b50af603093903a410971d3ff6d62b6e83779d
FredrickMbithi <fredrickmbithi000@gmail.com>
2025-12-06 05:15:27 +0300

    fix(book_class): correct __del__ message format to 'Deleting {title}'

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,20 +1,14 @@
 class Book:
-    def __init__(self,title,author,year):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
     def __del__(self):
-        print(f"'{self.title}' deleted")
+        print(f"Deleting {self.title}")
 
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
-    
-b1 = Book("1984", "George Orwell", 1949)
-
-print(b1) 
-b1   
-del b1      
